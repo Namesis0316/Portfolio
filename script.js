7d5edb7c10a935473d5eb5925a9b6f2dc786f1c7
indranil <ichakroberty@gmail.com>
2026-02-12 23:38:15 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Select all anchor links
+const links = document.querySelectorAll("a[href^='#']");
+
+links.forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute("href");
+        const targetSection = document.querySelector(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
